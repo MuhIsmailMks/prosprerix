414462909f831259fd827cc6807a9a66d39e552d
muhammad ismail <muhismailmks@gmail.com>
2025-02-10 06:31:17 +0800

    update

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
+    AOS.init({
+        once: true
+      });
+
+
     const musicButton = document.querySelector(".music");
     const musicIcon = document.querySelector(".rotate-animation");
     const audio = document.getElementById("backgroundMusic");
@@ -12,4 +17,29 @@ document.addEventListener("DOMContentLoaded", function () {
             musicIcon.classList.remove('rotate-animation')
         }
     });
+
+
+
+    document.addEventListener("DOMContentLoaded", function () {
+        const sidebar = document.querySelector(".image-right");
+        const footer = document.querySelector("footer"); // Gantilah dengan elemen footer atau batas bawah
+      
+        function checkPosition() {
+          let sidebarRect = sidebar.getBoundingClientRect();
+          let footerRect = footer.getBoundingClientRect();
+      
+          if (footerRect.top <= window.innerHeight) {
+            sidebar.style.position = "absolute";
+            sidebar.style.bottom = "0";
+            sidebar.style.top = "auto";
+          } else {
+            sidebar.style.position = "fixed";
+            sidebar.style.top = "20px";
+          }
+        }
+      
+        window.addEventListener("scroll", checkPosition);
+        checkPosition();
+      });
+      
 });
