28957ecfaa1f8ff3f9cbbdba3d069c0f58f01f6b
muhammad ismail <muhismailmks@gmail.com>
2025-02-10 15:16:59 +0800

    update

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,16 +5,18 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
     const musicButton = document.querySelector(".music");
-    const musicIcon = document.querySelector(".rotate-animation");
+    const musicIcon = document.querySelector(".music .rotate-animation");
     const audio = document.getElementById("backgroundMusic");
 
     musicButton.addEventListener("click", function () {
         if (audio.paused) {
             audio.play();  
             musicIcon.classList.add('rotate-animation')
+            musicButton.classList.remove('active')
         } else {
             audio.pause(); 
             musicIcon.classList.remove('rotate-animation')
+            musicButton.classList.add('active')
         }
     });
 
